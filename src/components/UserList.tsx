39f90a91df6a99482844399dca4a619355105b45
Seynaro <xamzatseynaro@gmail.com>
2021-05-20 15:46:15 +0300

    add UserList

---
+++ src/components/UserList.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+import { IUser } from '../types/types';
+
+interface UserListProps {
+    users: IUser[]
+}
+
+const UserList: FC<UserListProps> = ({users}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UserList;
